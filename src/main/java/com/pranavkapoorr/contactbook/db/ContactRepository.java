47ed3d9c636369991f8ac88ee5ae2b8c0ceca846
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-04-19 23:56:02 -0400

    synchronized manipulations

--- src/main/java/com/pranavkapoorr/contactbook/db/ContactRepository.java
+++ src/main/java/com/pranavkapoorr/contactbook/db/ContactRepository.java
@@ -11,34 +11,36 @@ import com.pranavkapoorr.contactbook.utilities.ContactUtils;
 @Repository
 public class ContactRepository implements ContactDao{
 	private static List<Contact> contacts;
-	private int id = 0;
+	private volatile int id = 0;
 	
 	public ContactRepository() {
 		contacts = new ArrayList<Contact>();
-		//contacts.add(new Contact(1,"Pranav","Kapoor","6476337891","6479937891","1665 Clark Blvd","11:30:11","12:30:44"));
-		//contacts.add(new Contact(2,"Sundar","Pichai","7488706094","7488706098","1402 Beverly Hills","11:30:20","12.30:50"));
 	}
 	
 	@Override
 	public String addContact(Contact contact) {
-		contact.setCreatedTime(getCurrentTimeStamp());//adding created time
 		boolean result = false;
-		contact.setId(id);
-		id++;
-		result = contacts.add(contact);
+		synchronized (this) {
+			contact.setCreatedTime(getCurrentTimeStamp());//adding created time
+			contact.setId(id);
+			id++;
+			result = contacts.add(contact);
+		}
 		return !result?"Failed to Add":"Added Successfully!";
 	}
 
 	@Override
 	public String updateContact(Contact contact) {
-		Contact originalContact = contacts.stream().filter(c->c.getId()==contact.getId()).findAny().orElse(null);
 		boolean result = false;
-		int index = contacts.indexOf(originalContact);
-		if(index != -1) {
-			contact.setCreatedTime(originalContact.getCreatedTime());
-			result = true;
-			contact.setUpdatedTime(getCurrentTimeStamp());//setting update time
-			contacts.set(index, contact);
+		synchronized (this) {
+			Contact originalContact = contacts.stream().filter(c->c.getId()==contact.getId()).findAny().orElse(null);
+			int index = contacts.indexOf(originalContact);
+			if(index != -1) {
+				contact.setCreatedTime(originalContact.getCreatedTime());
+				result = true;
+				contact.setUpdatedTime(getCurrentTimeStamp());//setting update time
+				contacts.set(index, contact);
+			}
 		}
 		return !result?"Requested Contact doesn't exist!":"Updated Successfully!";
 	}
@@ -61,11 +63,13 @@ public class ContactRepository implements ContactDao{
 
 	@Override
 	public String removeContact(int contactId) {
-		Contact contact = contacts.stream().filter(c->c.getId()==contactId).findAny().orElse(null);
 		boolean result = false;
-		if(contact!=null) {
-			result = contacts.remove(contact);
-		}
+		synchronized (this) {
+			Contact contact = contacts.stream().filter(c->c.getId()==contactId).findAny().orElse(null);
+			if(contact!=null) {
+				result = contacts.remove(contact);
+			}
+		}	
 		return !result?"Failed to Delete":"Deleted Successfully!";
 	}
 	
